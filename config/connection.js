353bab70e1ab5fdb82b24bc7267c4b831b60bef2
frankwichw <frankwichw@gmail.com>
2018-02-28 20:23:26 -0500

    jawsDB

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,18 @@
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "root",
-  database: "burgers_db",
-  // socketPath: "/var/run/mysqld/mysqld.sock"
-});
+if(process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else{
+  connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "root",
+    database: "burgers_db",
+  });
+};
+
+
 
 connection.connect(function(err) {
   if (err) {
